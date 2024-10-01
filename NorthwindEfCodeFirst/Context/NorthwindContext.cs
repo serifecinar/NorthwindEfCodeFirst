a69153ffc633f7d0dd52349566b0218c138c967a
serifeecinar@outlook.com <serifeecinar@outlook.com>
2024-10-01 15:23:58 +0300

    Add project files.

---
+++ NorthwindEfCodeFirst/Context/NorthwindContext.cs
@@ -0,0 +1,16 @@
+﻿using NorthwindEfCodeFirst.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindEfCodeFirst.Context;
+
+public class NorthwindContext:DbContext
+{
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Order> Orders { get; set; }
+
+}
---
+++ NorthwindEfCodeFirst/Program.cs
@@ -0,0 +1,19 @@
+﻿using NorthwindEfCodeFirst.Context;
+
+namespace NorthwindEfCodeFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(NorthwindContext northwindContext=new NorthwindContext())            
+            {
+                foreach (var customer in northwindContext.Customers)
+                {
+                    Console.WriteLine("Customer Name : {0}", customer.ContactName);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
