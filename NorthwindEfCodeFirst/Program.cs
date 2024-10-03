0f8a907ac70bf8c2f50588defdaac432ebe48ecd
serifeecinar@outlook.com <serifeecinar@outlook.com>
2024-10-03 16:53:05 +0300

    ekleme silme güncelleme

--- NorthwindEfCodeFirst/Program.cs
+++ NorthwindEfCodeFirst/Program.cs
@@ -1,10 +1,70 @@
 ﻿using NorthwindEfCodeFirst.Context;
+using NorthwindEfCodeFirst.Entities;
 
 namespace NorthwindEfCodeFirst
 {
     class Program
     {
         static void Main(string[] args)
+        {
+            //One();
+            //Two();
+            //Three();
+            //Four();
+            using (var northwindContext = new NorthwindContext())
+            {
+                Customer customer = northwindContext.Customers.Find("ENGİN");
+                customer.Country = "Turkey";
+                northwindContext.SaveChanges();
+            }
+                Console.ReadLine();
+        }
+
+        private static void Four()
+        {
+            using (var northwindContext = new NorthwindContext())
+            {
+                Order order = northwindContext.Orders.Find(1);
+                northwindContext.Orders.Remove(order);
+                northwindContext.SaveChanges();
+            }
+        }
+
+        private static void Three()
+        {
+            using (var northwindContext = new NorthwindContext())
+            {
+                Customer customer = northwindContext.Customers.Find("ENGİN");
+                customer.Orders.Add(new Order
+                {
+                    OrderID = 1,
+                    OrderDate = DateTime.Now,
+                    ShipCity = "Ankara",
+                    ShipCountry = "Türkiye"
+                });
+                northwindContext.SaveChanges();
+            }
+        }
+
+        private static void Two()
+        {
+            using (var northwindContext = new NorthwindContext())
+            {
+                var customer = new Customer
+                {
+                    CustomerID = "ENGİN",
+                    City = "Ankara",
+                    CompanyName = "YazılımDevi.Com",
+                    ContactName = "Engin Demiroğ",
+                    Country = "Türkiye"
+                };
+
+                northwindContext.Customers.Add(customer);
+                northwindContext.SaveChanges();
+            }
+        }
+
+        private static void One()
         {
             using(NorthwindContext northwindContext=new NorthwindContext())            
             {
@@ -13,7 +73,8 @@ namespace NorthwindEfCodeFirst
                     Console.WriteLine("Customer Name : {0}", customer.ContactName);
                 }
             }
-            Console.ReadLine();
         }
+        
+
     }
 }
